14b9a9887c9bc9e1aeeab7102fa37427fe37ada1
ramnalawade1986 <56192892+ramnalawade1986@users.noreply.github.com>
2019-10-15 13:53:21 +0530

    2
    2

--- Routes/verifytoken.js
+++ Routes/verifytoken.js
@@ -6,7 +6,7 @@ module.exports = function (req, res, next) {
     try {
         token = token.split("Bearer ");
         token = token[1];
-        const verified = jwt.verify(token,'sfhsfjdsfljl');
+        const verified = jwt.verify(token,process.env.TOKEN_SECRET);
         req.user = verified;
         next();
     } catch (err) {
